59e2c326c0831c0bd003d092cf4e8ab4c163edf4
tabe-ht <tabehellosk@gmail.com>
2025-11-14 20:41:49 +0900

    onaka suita

--- testGit/src/test/GitTest.java
+++ testGit/src/test/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Good Morning GitHub!");
 		System.out.println("Hello GitHub!");
+		System.out.println("onaka suita");
 	}
 }
